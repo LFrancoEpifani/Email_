f2b49ac71502cb5f1a97d8c2ea9248c0cd908912
Edmondo Di Tucci <Snarchio@gmail.com>
2024-08-13 21:20:03 +0200

    remove args from script

--- remote-script.sh
+++ remote-script.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
-Help()
-{  # Display Help
-   echo 'Syntax: scriptTemplate [-p|h]'
-   echo 'options:'
-   echo 'p  install Path'
-   echo 'h  print this Help'
-   echo
-}
+# Help()
+# {  # Display Help
+#    echo 'Syntax: $(basename "$0") [-p|h]'
+#    echo 'options:'
+#    echo 'p  install Path'
+#    echo 'h  print this Help'
+#    echo
+# }
 
-while getopts ':p:h' option; do
-  case $option in
-    h) # display Help
-	    Help
-        exit;;
-    p) # Enter a name
-        path=$OPTARG;;		 
-    \?) # Invalid option
-	    echo 'Error: Invalid option'
-        exit;;
-  esac
-done
+# while getopts ':p:h' option; do
+#   case $option in
+#     h) # display Help
+# 	    Help
+#         exit;;
+#     p) # Enter a name
+#         path=$OPTARG;;		 
+#     \?) # Invalid option
+# 	    echo 'Error: Invalid option'
+#         exit;;
+#   esac
+# done
 
-if [ ! -d '$path' ]; then
-  echo '$path does not exist'
-  exit 1
-fi
+# if [ ! -d '$path' ]; then
+#   echo '$path does not exist'
+#   exit 1
+# fi
 
-cd $path
+# cd $path
+
+cd /var/www/mail-dashboard-svelte
 
 printf '\nInstall node depelndencies'
 npm install
