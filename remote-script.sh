3b02325e4b4dd501362dbb3cc35112bd5e9d5fb0
Edmondo Di Tucci <Snarchio@gmail.com>
2024-08-13 21:01:25 +0200

    more general script

--- remote-script.sh
+++ remote-script.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
-echo 'Install node depelndencies'
+Help()
+{  # Display Help
+   echo "Syntax: scriptTemplate [-p|h]"
+   echo "options:"
+   echo "p  install Path"
+   echo "h  print this Help"
+   echo
+}
+
+while getopts ":p:h" option; do
+  case $option in
+    h) # display Help
+	    Help
+        exit;;
+    p) # Enter a name
+        path=$OPTARG;;		 
+    \?) # Invalid option
+	    echo "Error: Invalid option"
+        exit;;
+  esac
+done
+
+if [ ! -d "$path" ]; then
+  echo "$path does not exist"
+  exit 1
+fi
+
+cd $path
+
+printf '\nInstall node depelndencies'
 npm install
 
-echo 'Install python depelndencies'
+printf '\nInstall python depelndencies'
 pip install -r requirements.txt
 
-echo 'Restart instance on pm2'
+printf '\nRestart instance on pm2'
 pm2 restart mail-dashboard
