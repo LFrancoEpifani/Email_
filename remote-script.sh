37c01229707490851b5a3202023aafbd07424f9d
Edmondo Di Tucci <Snarchio@gmail.com>
2024-08-13 21:30:12 +0200

    get and print parameters

--- remote-script.sh
+++ remote-script.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-# Help()
-# {  # Display Help
-#    echo 'Syntax: $(basename "$0") [-p|h]'
-#    echo 'options:'
-#    echo 'p  install Path'
-#    echo 'h  print this Help'
-#    echo
-# }
+Help()
+{  # Display Help
+   echo 'Syntax: $(basename "$0") [-p|h]'
+   echo 'options:'
+   echo 'p  install Path'
+   echo 'h  print this Help'
+   echo
+}
 
-# while getopts ':p:h' option; do
-#   case $option in
-#     h) # display Help
-# 	    Help
-#         exit;;
-#     p) # Enter a name
-#         path=$OPTARG;;		 
-#     \?) # Invalid option
-# 	    echo 'Error: Invalid option'
-#         exit;;
-#   esac
-# done
+while getopts ':p:h' option; do
+  case $option in
+    h) # display Help
+	    Help
+        exit;;
+    p) # Enter a name
+        path=$OPTARG;;		 
+    \?) # Invalid option
+	    echo 'Error: Invalid option'
+        exit;;
+  esac
+done
+
+echo 'parameter is $path'
 
 # if [ ! -d '$path' ]; then
 #   echo '$path does not exist'
@@ -36,5 +38,5 @@ cd /var/www/mail-dashboard-svelte
 # printf '\nInstall python depelndencies'
 # pip install -r requirements.txt
 
-printf '\nRestart instance on pm2'
-pm2 restart mail-dashboard
+# printf '\nRestart instance on pm2'
+# pm2 restart mail-dashboard
