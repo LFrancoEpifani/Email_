efc10e07e823199148ee86a7e4a93bb456feaaac
Edmondo Di Tucci <snarchio@gmail.com>
2024-08-14 13:36:44 +0200

    update pipeline

---
+++ bitbucket-pipelines_remote-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ -z "${INSTALL_PATH}" ]]; then
+  echo "Missing INSTALL_PATH environment variable"
+  exit 1
+fi
+
+echo "Change current path to $INSTALL_PATH"
+cd $INSTALL_PATH
+
+printf "Install node dependencies\n"
+npm install
+
+printf "Install python dependencies\n"
+pip install -r requirements.txt
+
+printf "Restart instance on pm2\n"
+pm2 restart mail-dashboard
